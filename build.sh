f0d01e8e98fe310d9032365ba3004928a81ebf7d
ericthelemur <owenconnors1@gmail.com>
2023-11-08 01:54:45 +0000

    Use bootstrap as submodule

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 export SCRIPT_DIR=$(dirname "$(realpath $0)")
 cd $SCRIPT_DIR && echo "Running in $SCRIPT_DIR"
-if [ -z $NO_PULL ]; then git pull --recurse-submodules; fi
+if [ -z $NO_PULL ]; then git pull --force --recurse-submodules; fi
 
 # Build main
 ./zola build --base-url https://warwickspeed.run --output-dir ../build --force
