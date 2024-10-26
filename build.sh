f40b55bb289e1d229d18b66d3b8689eced921a6c
ericthelemur <owenconnors1@gmail.com>
2024-10-26 17:04:14 +0100

    Add draft workflow

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -e
 
 export SCRIPT_DIR=$(dirname "$(realpath $0)")
 cd $SCRIPT_DIR && echo "Running in $SCRIPT_DIR"
+
+git checkout master
 if [ -z $NO_PULL ]; then 
     git fetch --all --prune
     git reset --hard origin/master
